0dfa32d1773f9954bc2cd8f7216c95bf1a5a5c55
YoungJae <142131436+rhsiddlsidhd@users.noreply.github.com>
2023-12-12 01:15:19 +0900

    [level unrated] Title: 홀수 vs 짝수, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,17 @@
+function solution(num_list) {
+  let sumEven = 0;
+  let sumOdd = 0;
+
+  for (let i = 0; i < num_list.length; i++) {
+    if (i % 2 === 0) {
+      
+      sumEven += num_list[i];
+    } else {
+      
+      sumOdd += num_list[i];
+    }
+  }
+
+  
+  return Math.max(sumEven, sumOdd);
+}
